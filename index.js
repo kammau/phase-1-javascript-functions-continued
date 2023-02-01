1ddc6558e7f8ec4c825234518dc64d1f46626aa1
Kamryn Smith <programersign@gmail.com>
2023-02-01 12:34:28 -0800

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(inPut) {
+    if (inPut != undefined) {
+        return `This Saturday, I want to ${inPut}!`
+    } else {
+        return "This Saturday, I want to roller-skate!"
+    }
+}
+
+const mondayWork = function(work) {
+    if (work != undefined) {
+        return `This Monday, I will ${work}.`
+    } else {
+        return "This Monday, I will go to the office."
+    }
+}
+
+function wrapAdjective(inPut = "*") {
+    return str = function(str2) {
+        return `You are ${inPut}${str2}${inPut}!`
+    }
+}
